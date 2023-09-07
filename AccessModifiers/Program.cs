bec905ba456ab3b24b5c9e46023a5fa2ada2b7c8
Atalay Yaman <atalayyaman770@gmail.com>
2023-09-07 16:30:17 +0300

    Private&Protected

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        private int Id { get; set; }
+
+        public void Save()
+        {
+            Id = Id + 1;
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+    class Student: Customer
+    {
+        public void Save2()
+        {
+            
+        }
+    }
+}
