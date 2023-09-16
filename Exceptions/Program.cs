57466a2fbf4a5f48c5e8d58098e97a81cb049278
Atalay Yaman <atalayyaman770@gmail.com>
2023-09-16 18:27:41 +0300

    ExceptionHandling

---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] {"Ata","Yaman","Azra"};
+                students[3] = "Ahmet";
+            }
+            //catch (IndexOutOfRangeException exception)
+            //{
+            //    Console.WriteLine(exception.Message);
+            //}
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
