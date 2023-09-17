488913e5d00390834a2d0fc88ac430dc7dbadf1d
Atalay Yaman <atalayyaman770@gmail.com>
2023-09-17 14:02:47 +0300

    WorkWithEntityFramework2

--- EntityFrameworkDemo/ProductDal.cs
+++ EntityFrameworkDemo/ProductDal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,41 @@ namespace EntityFrameworkDemo
 {
     internal class ProductDal
     {
-        
+        public List<Product> GetAll()
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void Add(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                //context.Products.Add(product);//Bu şekilde de olabilir.
+                var entity = context.Entry(product);
+                entity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(Product product)
+        {
+            using (ETradeContext context = new ETradeContext())
+            {
+                var entity = context.Entry(product);
+                entity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
     }
 }
