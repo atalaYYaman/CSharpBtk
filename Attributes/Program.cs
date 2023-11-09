df4e7f02e4e72d4d2eaef7bea055a18e266e6586
Atalay Yaman <atalayyaman770@gmail.com>
2023-11-09 14:43:08 +0300

    Work with Attribute

---
+++ Attributes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1,LastName = "Yaman", Age = 20};
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+
+    [ToTable("Customer")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Dont use Add,instead use AddNew Method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!!", customer.Id,customer.FirstName ,customer.LastName, customer.Age);
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!!", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true /*allowMultiple = true,you can use more than one times*/)]
+    class RequiredPropertyAttribute:Attribute
+    {
+      
+    }
+
+    [AttributeUsage(AttributeTargets.Class /* | AttributeTargets.Field  */)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
