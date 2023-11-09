beeabbdf9aae693e99183f2c4921706fa48b9642
Atalay Yaman <atalayyaman770@gmail.com>
2023-11-09 21:07:01 +0300

    Work with Reflection 1
    We have some bugs in the ınvoke line next commit will be fixed.

---
+++ Reflection/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortıslem = new DortIslem(2,3);
+            //Console.WriteLine(dortıslem.topla2());
+            //Console.WriteLine(dortıslem.topla(3,4));
+
+            var tip = typeof(DortIslem);
+
+            //DortIslem dortıslem = (DortIslem)Activator.CreateInstance(tip,5,6);
+            //Console.WriteLine(dortıslem.topla(4,5));
+            //Console.WriteLine(dortıslem.topla2());
+
+            var instance = Activator.CreateInstance(tip,5,6);
+
+            MethodInfo methodInfo = instance.GetType().GetMethod("Topla2");
+            Console.WriteLine(methodInfo.Invoke(instance));
+
+
+            Console.ReadLine();
+            
+        }
+    }
+
+    public class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+
+        public DortIslem(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+
+        }
+        
+
+        public int topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        
+        public int carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+
+        public int carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+    }
+}
